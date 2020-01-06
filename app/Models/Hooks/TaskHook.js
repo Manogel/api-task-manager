49d29b4125d935d4b2068a360929258ad79f0419
Manogel <manoel_ggomes@hotmail.com>
2020-01-06 12:09:57 -0300

    jobs with adonis-kue

--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,30 +1,16 @@
 'use strict'
-const Mail = use('Mail')
-const Helpers = use('Helpers')
+
+const Kue = use('Kue')
+const Job = use('App/Jobs/NewTaskMail')
 
 const TaskHook = exports = module.exports = {}
 
 TaskHook.sendNewTaskMail = async (taskInstance) => {
-  try {
-    if (!taskInstance.user_id && !taskInstance.dirty.user_id) return
+  if (!taskInstance.user_id && !taskInstance.dirty.user_id) return
 
-    const { email, username } = await taskInstance.user().fetch()
-    const file = await taskInstance.file().fetch()
-    const { title } = taskInstance
-    await Mail.send(
-      ['emails.new_task'],
-      { username, title, hasAttachment: !!file },
-      message => {
-        message.to(email).from('manoelgomes53@gmail.com', 'Manoel').subject('Nova tarefa para você')
+  const { email, username } = await taskInstance.user().fetch()
+  const file = await taskInstance.file().fetch()
+  const { title } = taskInstance
 
-        if (file) {
-          message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-            filename: file.name
-          })
-        }
-      }
-    )
-  } catch (e) {
-    console.log(e)
-  }
+  Kue.dispatch(Job.key, { email, username, file, title }, { attempts: 3 })
 }
